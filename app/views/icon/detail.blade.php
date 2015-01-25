61c0ec2c4f8d626d96be831de904242ea444d6a8
Hans Zhang <ezburg@gmail.com>
2015-01-25 08:35:44 +0100

    Added the android icon layout

--- app/views/icon/detail.blade.php
+++ app/views/icon/detail.blade.php
@@ -6,18 +6,36 @@
     #tp_tabs li a {
         font-size: 16px;
     }
-    #tp_ios .icons-row {
-        margin-bottom: 35px;
-    }
     #tp_ios .icons-row .icon-row {
         vertical-align: middle;
         height: 56px;
         line-height: 56px;
     }
-    #tp_ios .icons-row .description {
+    #tp_contents {
+        padding:30px 70px 0;
+    }
+    #tp_contents .icons-row {
+        margin-bottom: 45px;
+    }
+    #tp_contents .icons-col {
+        font-weight: normal;
+        text-align: center;
+        padding-top:50px;
+        color: #999;
+        font-size:11px;
+    }
+    #tp_contents .description {
         border-top: 1px solid silver;
         margin-top: 5px;
         padding-top: 5px;
+    }
+    #tp_contents #tp_android .description {
+        border-top-width: 0;
+        line-height: 1.8em;
+        margin-top: 0;
+    }
+    #tp_contents #tp_android .icons-row {
+        margin-bottom: 15px;
     }
 </style>
 
@@ -42,11 +60,13 @@
     <ul class="nav nav-tabs nav-justified" role="tablist" id="tp_tabs">
         <li role="presentation" class="active"><a href="#tp_ios" aria-controls="tp_ios" role="tab" data-toggle="tab">iOS</a></li>
         <li role="presentation"><a href="#tp_android" aria-controls="tp_android" role="tab" data-toggle="tab">Android</a></li>
+        @if (isset($withExtra))
         <li role="presentation"><a href="#tp_winphone" aria-controls="tp_winphone" role="tab" data-toggle="tab">Windows Phone</a></li>
         <li role="presentation"><a href="#tp_webapp" aria-controls="tp_webapp" role="tab" data-toggle="tab">Web App</a></li>
+        @endif
     </ul>
 
-    <div class="tab-content" style="padding:30px 70px 0">
+    <div class="tab-content" id="tp_contents">
         <div role="tabpanel" class="tab-pane fade in active" id="tp_ios">
             <div class="row">
                 <div class="col-md-6" style="background: url('/img/iphone6.png') no-repeat; height:743px; position: relative">
@@ -55,7 +75,7 @@
                         <p style="color: #ccc;font-weight: normal;font-size: 12px;text-align: center;padding-top: 4px;">您的应用</p>
                     </div>
                 </div>
-                <div class="col-md-6" style="font-weight: normal; text-align: center; padding-top:50px; color: #999; font-size:11px">
+                <div class="col-md-6 icons-col">
                     <div class="row icons-row">
                         <div class="col-sm-4">
                             <div class="row icon-row">
@@ -242,24 +262,82 @@
             </div>
         </div>
         <div role="tabpanel" class="tab-pane fade" id="tp_android">
-            <div class="row">
+            <div class="row" style="padding:0 50px">
                 <div class="col-md-6" style="background: url('/img/nexus5.png') no-repeat; height:743px;">
-
+                    <div style="position: absolute; left:50px; top:180px;">
+                        <img src="/files/<?php echo $design->getFilePath(); ?>" alt="" style="width:64px; height:64px;" />
+                        <p style="color: #ccc;font-weight: normal;font-size: 12px;text-align: center;padding-top: 8px; font-weight:bold">您的应用</p>
+                    </div>
                 </div>
-                <div class="col-md-6"></div>
+                <div class="col-md-6 icons-col">
+                    <div class="row icons-row">
+                        <div class="col-sm-6">
+                            <div style="line-height: 72px; vertical-align: middle">
+                                <img src="/files/<?php echo $design->getFilePath(); ?>" alt="" style="width:48px; height:48px;" />
+                            </div>
+                            <div class="description">
+                                1x<br/>
+                                MDPI
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div>
+                                <img src="/files/<?php echo $design->getFilePath(); ?>" alt="" style="width:72px; height:72px;" />
+                            </div>
+                            <div class="description">
+                                1.5x<br/>
+                                HDPI
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row icons-row">
+                        <div class="col-sm-6">
+                            <div style="line-height: 144px; vertical-align: middle">
+                                <img src="/files/<?php echo $design->getFilePath(); ?>" alt="" style="width:96px; height:96px;" />
+                            </div>
+                            <div class="description">
+                                2x<br/>
+                                XDPI
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div>
+                                <img src="/files/<?php echo $design->getFilePath(); ?>" alt="" style="width:144px; height:144px;" />
+                            </div>
+                            <div class="description">
+                                3x<br/>
+                                XXDPI
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row icons-row">
+                        <div class="col-sm-12">
+                            <div>
+                                <img src="/files/<?php echo $design->getFilePath(); ?>" alt="" style="width:192px; height:192px;" />
+                            </div>
+                            <div class="description">
+                                4x<br/>
+                                XXXDPI
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row icons-row" style="padding-top: 30px">
+                        <div class="col-sm-12">
+                            以及
+                            <a href="#"><span class="label label-info" style="font-size:13px;">Google Play 图标</span></a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+        @if (isset($withExtra))
         <div role="tabpanel" class="tab-pane fade" id="tp_winphone">
-            <div class="row">
-                <div class="col-md-6" style="background: url('/img/nexus5.png') no-repeat; height:743px;">
 
-                </div>
-                <div class="col-md-6"></div>
-            </div>
         </div>
         <div role="tabpanel" class="tab-pane fade" id="tp_webapp">
 
         </div>
+        @endif
     </div>
 
 </div>
--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="favicon.ico">
+    <link rel="icon" href="/favicon.ico">
 
     <title><?php echo Config::get('constants.site_name') . ' - ' . Config::get('constants.slogan') ?></title>
 
