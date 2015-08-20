a8eea76b7e957372f6159c9926b3283e15ef08b3
Hans Zhang <ezburg@gmail.com>
2015-08-20 17:10:39 +0200

    Added the orientation configuration boxes.

--- app/views/splash/home.blade.php
+++ app/views/splash/home.blade.php
@@ -3,17 +3,17 @@
 @section('content')
 <form class="jumbotron form-horizontal" action="/icon/upload" method="post">
     <div class="row">
-        <div class="col-md-9">
+        <div class="col-md-8">
             <h2 style="margin-bottom: 40px; margin-top: 0;">一键生成iOS不同尺寸的启动图片</h2>
             <div class="form-group">
-                <label for="inputLogoFile" class="col-sm-1 control-label">Logo: </label>
-                <div class="col-sm-11">
+                <label for="inputLogoFile" class="col-sm-2 control-label text-left">Logo: </label>
+                <div class="col-sm-10">
                     <input type="file" class="form-control" id="inputLogoFile">
                 </div>
             </div>
             <div class="form-group">
-                <label class="col-sm-1 control-label">背景:</label>
-                <div class="col-sm-11">
+                <label class="col-sm-2 control-label text-left">背景:</label>
+                <div class="col-sm-10">
                     <div class="form-group">
                         <label class="col-sm-2 control-label text-left">
                             <input type="radio" id="inputBgRadioColor" name="bgRadio" checked>
@@ -29,9 +29,22 @@
                             <label for="inputBgRadioImage">图片</label>
                         </label>
                         <div class="col-sm-10">
-                            <input type="file" class="form-control" id="inputBgFile">
+                            <input type="file" class="form-control" id="inputBgFile" disabled>
                         </div>
                     </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-2 control-label text-left">方向:</label>
+                <div class="col-sm-10">
+                    <label class="control-label" style="margin-right: 20px">
+                        <input type="radio" name="orientation" id="orientation_portrait" value="portrait" checked/>
+                        <label for="orientation_portrait">竖屏</label>
+                    </label>
+                    <labe class="control-label">
+                        <input type="radio" name="orientation" id="orientation_landscape" value="landscape"/>
+                        <label for="orientation_landscape">横屏</label>
+                    </labe>
                 </div>
             </div>
             <div style="margin-top: 30px">
@@ -42,8 +55,8 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-3">
-            <div id="jumbotron_img_box" style="margin-top:0; width: auto; height: auto;">
+        <div class="col-md-4">
+            <div id="jumbotron_img_box" class="splash">
                 <img src="/img/iphone_xs.png" alt="" id="splash_preview_phone"/>
                 <div id="splash_preview_bg" style="position: absolute; background-color: #0073d1; width: 139px; height: 248px; top: 37px; left: 11px;"></div>
                 <div id="jumbotron_img_loading">
--- public/js/splash.js
+++ public/js/splash.js
@@ -2,7 +2,11 @@ $(function(){
     var generate_splash = $("#generate_splash");
     if (generate_splash.length) {
         var bg = $("#splash_preview_bg");
-        $('#inputBgColor').colpick({
+
+        var inputBgColor = $("#inputBgColor");
+        var inputBgFile = $("#inputBgFile");
+
+        inputBgColor.colpick({
             layout: 'rgbhex',
             submit: false,
             color: '#0073d1',
@@ -14,6 +18,36 @@ $(function(){
             }
         }).keyup(function(){
             $(this).colpickSetColor(this.value);
+        });
+
+        var radioColor = $("#inputBgRadioColor");
+        radioColor.on('click', function(){
+            inputBgColor.removeAttr('disabled');
+            inputBgFile.attr('disabled', 'disabled');
+
+            inputBgColor.focus().click();
+        });
+
+        var radioImage = $("#inputBgRadioImage");
+        radioImage.on('click', function(){
+            inputBgFile.removeAttr('disabled');
+            inputBgColor.attr('disabled', 'disabled');
+
+            setTimeout(function(){
+                inputBgFile.click();
+            }, 100);
+        });
+
+        var orientation_portrait = $("#orientation_portrait");
+        var orientation_landscape = $("#orientation_landscape");
+        var jumbotron_img_box = $("#jumbotron_img_box");
+
+        orientation_portrait.on('click', function(){
+            jumbotron_img_box.removeClass('landscape');
+        });
+
+        orientation_landscape.on('click', function(){
+            jumbotron_img_box.addClass('landscape');
         });
     }
 });
