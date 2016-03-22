7cb3f35edcc8dfee4df9e1b92485ac464aefce93
Hans Zhang <ezburg@gmail.com>
2016-03-22 03:28:53 +0100

    Fixed the bug that the comment box is missing from about page when viewing not at first time by opening a new page.

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -54,10 +54,10 @@
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
                 <li ng-class="stateCls('home')"><a href="#/home">首页</a></li>
-                <li ng-class="stateCls('ios8')"><a href="#">iOS 8图标</a></li>
-                <li ng-class="stateCls('webapp')"><a href="#">WebApp图标</a></li>
+                <li ng-class="stateCls('ios')"><a href="#/ios" target="_blank">iOS 9图标</a></li>
+                <li ng-class="stateCls('webapp')"><a href="#/webapp" target="_blank">WebApp图标</a></li>
                 <li><a href="http://wuruihong.com" target="_blank">睿鸿游戏</a></li>
-                <li ng-class="stateCls('about')"><a href="#/about">关于我们</a></li>
+                <li ng-class="stateCls('about')"><a href="#/about" target="_blank">关于我们</a></li>
             </ul>
         </div>
     </div>
