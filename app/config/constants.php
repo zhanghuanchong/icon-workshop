0929f079c555425cd9aedf6fad1bf3d4ee978072
Hans Zhang <ezburg@gmail.com>
2016-05-02 15:32:28 +0800

    compile

--- app/config/constants.php
+++ app/config/constants.php
@@ -4,7 +4,7 @@ return array(
 
     'site_name' => '图标工场',
     'slogan' => '移动应用图标生成工具，一键生成所有尺寸的应用图标',
-    'version' => '2.8',
+    'version' => '2.9',
     'platforms' => [
         'ios' => 'iOS',
         'android' => 'Android',
