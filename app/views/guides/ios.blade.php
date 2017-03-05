0de05efd51b19770109a47c5cf8b6b760c671b6b
Hans Zhang <ezburg@gmail.com>
2017-03-06 00:32:51 +0800

    压缩图片

--- app/views/guides/ios.blade.php
+++ app/views/guides/ios.blade.php
@@ -5,15 +5,15 @@
             <div class="col-sm-9">
                 <h1>使用教程 - {{ $platform }}</h1>
                 <h3>第一步：上传并生成图片</h3>
-                <img class="normal-image" src="/img/guide/ios_1.png" alt="">
+                <img class="normal-image" src="/img/guide/ios_1.jpg" alt="">
                 <h3>第二步：下载图标</h3>
-                <img class="normal-image" src="/img/guide/ios_2.png" alt="">
+                <img class="normal-image" src="/img/guide/ios_2.jpg" alt="">
                 <h3>第三步：集成到Xcode</h3>
                 <p>删除项目文件Assets.xcassets中原有的AppIcon，将下载的压缩包中的AppIcon.appiconset拖放进去。你也可以在Finder中覆盖这个文件。</p>
-                <img class="normal-image" src="/img/guide/ios_3.png" alt="">
+                <img class="normal-image" src="/img/guide/ios_3.jpg" alt="">
                 <h3>第四步：编译完成</h3>
                 <p>Enjoy!</p>
-                <img class="normal-image" src="/img/guide/ios_4.png" alt="">
+                <img class="normal-image" src="/img/guide/ios_4.jpg" alt="">
             </div>
 
             <div class="col-sm-3">
