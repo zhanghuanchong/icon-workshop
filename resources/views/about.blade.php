4b29009efb08a1d4a596513cd0244fb61acd2307
Hans Zhang <ezburg@gmail.com>
2018-05-06 00:32:34 +0800

    github buttons

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,17 +4,31 @@
         <div class="row">
             <div class="col-md-4">
                 <h1>关于我们</h1>
-                <p>
-                    睿鸿游戏, 教育类移动游戏专家.
-                </p>
-                <p>
+                <p class="mt-10">
                     欢迎您对本工具提出宝贵的意见和建议!
                 </p>
+                <p class="mt-10 lh-28 flex">
+                    如果帮到你了，欢迎
+                    <span class="ml-10">
+                        <a class="github-button" href="https://github.com/zhanghuanchong/icon-workshop"
+                           data-size="large" data-show-count="true"
+                           aria-label="Star zhanghuanchong/icon-workshop on GitHub">Star</a>
+                    </span>
+                    <span class="ml-10">
+                        <a class="github-button" href="https://github.com/zhanghuanchong/icon-workshop/issues"
+                           data-size="large" data-show-count="true"
+                           aria-label="Issue zhanghuanchong/icon-workshop on GitHub">Issue</a>
+                    </span>
+                </p>
                 <ul style="list-style:none">
-                    <li><i class="fa fa-envelope-o"></i>: <a href="mailto:admin@wuruihong.com">admin@wuruihong.com</a></li>
+                    <li><i class="fa fa-envelope-o"></i>
+                        <a href="mailto:admin@wuruihong.com" class="ml-10">admin@wuruihong.com</a></li>
+                    <li class="mt-10"><i class="fa fa-road"></i>
+                        <a href="https://github.com/zhanghuanchong/icon-workshop/projects/1"
+                           class="ml-10" target="_blank">开发路线</a></li>
                 </ul>
             </div>
-            <div class="col-md-8">
+            <div class="col-md-8" style="padding-top: 70px;">
                 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                 <!-- 关于页 -->
                 <ins class="adsbygoogle"
