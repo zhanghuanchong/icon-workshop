6100230ccea719516d703ad81ccc5827c91a2cd7
Hans <zhanghc@shinetechchina.com>
2019-02-15 15:16:33 +0800

    Updated site title/keyword/description

--- config/constants.php
+++ config/constants.php
@@ -3,8 +3,8 @@
 return array(
 
     'site_name' => '图标工场',
-    'slogan' => '移动应用图标生成工具，一键生成所有尺寸的应用图标',
-    'version' => '3.0',
+    'slogan' => '移动应用图标/启动图生成工具，一键生成所有尺寸的应用图标/启动图',
+    'version' => '4.0',
     'platforms' => [
         'ios' => 'iOS',
         'android' => 'Android',
