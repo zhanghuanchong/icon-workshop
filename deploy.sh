49866a150e490f539e843479a7221efc6dfc2045
Hans <zhanghc@shinetechchina.com>
2019-03-14 00:51:49 +0800

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+git pull
+
+composer install
+
+php artisan migrate
+php artisan view:clear
+php artisan route:clear
+php artisan config:clear
+php artisan clear-compiled
+#
+service php-fpm-71 reload
